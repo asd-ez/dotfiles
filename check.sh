087c56566c848edf1be90152ebfa8fd9ae64954b
Mykyta Lazurenko <nikita.lazurenko.d@gmail.com>
2023-06-18 00:39:40 +0300

    chore: update check script 2023-06-18 (#3)

--- check.sh
+++ check.sh
@@ -43,12 +43,12 @@ git commit -m "$commit_message"
 git push origin "$branch_name"
 
 # gh pr create --label enhancement --assignee @me --web
-hub pull-request -l enhancement --assign mykyta-lazurenko --no-edit
+hub pull-request --base main -l enhancement --assign mykyta-lazurenko --no-edit
 
 # pull_request_url=$(hub pr show)
 # echo "Pull request created: $pull_request_url"
 
 gh pr merge --squash --delete-branch
-git checkout master
+git checkout main
 git pull
 git branch -d "$branch_name"
