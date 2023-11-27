018842474d74205b292d6820b1725f18feadac5c
Mykyta Lazurenko <nikita.lazurenko.d@gmail.com>
2023-11-27 22:16:36 +0200

    feat: update 2023-11-27 (#10)

--- cp.sh
+++ cp.sh
@@ -1,11 +1,16 @@
 # tmux
-cp ~/.tmux.conf tmux/
+cp ~/.tmux.conf .config/tmux/
 
 # zsh
-cp ~/.zshrc zsh/
+cp ~/.zshrc .config/zsh
 
 # bash
-cp ~/.profile bash/
+cp ~/.profile .config/bash-profile
 
 # asdf
-cp ~/.tool-versions asdf/
+cp ~/.tool-versions .config/asdf
+
+# nvim
+cp -a ~/.config/nvim/lua .config/nvim
+cp ~/.config/nvim/init.lua .config/nvim/init.lua
+cp ~/.config/nvim/lazyvim.json .config/nvim/lazyvim.json
